9c3c0cfe271c791471d4e258bf3262e45fb63f97
Shobhit Singh <Shobhit.Singh@rsystems.com>
2017-01-17 15:35:33 +0530

    commit to master branch

--- WebAnalytics/src/com/rsil/WebAnalyticsMain.java
+++ WebAnalytics/src/com/rsil/WebAnalyticsMain.java
@@ -3,12 +3,7 @@ package com.rsil;
 public class WebAnalyticsMain {
 
 	public static void main(String[] args) {
-		System.out.println("Main methods!");
-		System.out.println("Next to first line!");
-		
-		System.out.println("Again........."); 
-		
-		System.out.println("Task1-Testt branch.....");
+		System.out.println("Master Branch");
 	}
 
 }
